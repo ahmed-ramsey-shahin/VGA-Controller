30d03a26731d0b6ced98720be87651da95e8ed66
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2024-06-01 05:15:39 +0300

    Creating the clock divider

--- ClockDivider.v
+++ ClockDivider.v
@@ -1,22 +1,14 @@
 `timescale 1ns/1ps
 
-module ClockDivider #(parameter COUNTER_END_VALUE=5) (
+module ClockDivider (
     input clk, reset_n,
     output clk_div
 );
-    wire [$clog2(COUNTER_END_VALUE):0] q;
-    
-    Counter #($clog2(COUNTER_END_VALUE) + 1) counter (
-        .clk(clk),
-        .reset_n(reset_n),
-        .q(q)
-    );
-
     DFlipFlop dff (
         .clk(clk),
         .reset_n(reset_n),
         .q(clk_div),
         .d(~clk_div),
-        .en((q == COUNTER_END_VALUE))
+        .en(1'b1)
     );
 endmodule
